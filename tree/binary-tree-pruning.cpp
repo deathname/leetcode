aaf2e44f2649e5e302ab2e030708357c33536d40
Shakti Raj <shakti.rajpandey@flipkart.com>
2019-07-02 01:50:11 +0530

    binary-tree-pruning

---
+++ tree/binary-tree-pruning.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if(!root){
+            return NULL;
+        }
+        if(!root->left && !root->right){
+            if (root->val == 0){
+                return NULL;
+            }
+            return root;
+        }
+        
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+        
+        if(!root->left && !root->right && root->val==0){
+            return NULL;
+        }
+        return root;
+    }
+};
